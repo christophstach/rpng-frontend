5b495fbc972d8a83f316d842208f87675d576ea4
Christoph Stach <christoph.stach@gmail.com>
2018-07-26 22:44:55 +0200

    added profile dummy page

--- src/app/auth/components/profile/profile.component.ts
+++ src/app/auth/components/profile/profile.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+  profileForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly fb: FormBuilder
+  ) {
+    this.createForm();
+  }
 
-  ngOnInit() {
+  createForm() {
+    this.profileForm = this.fb.group({
+      email: [
+        {value: '', disabled: true},
+        [Validators.required, Validators.email]
+      ],
+      username: [
+        {value: '', disabled: true},
+        Validators.required
+      ],
+      firstName: '',
+      lastName: ''
+    });
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      this.authService.register(this.profileForm.getRawValue())
+        .subscribe((response) => {
+          alert('Successfully registered');
+        });
+    }
   }
 
 }
